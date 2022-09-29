e2305327a125a6b8294878045c084517fdc925d2
philipp-schaffer <sch19694@spengergasse.at>
2022-09-29 13:59:17 +0200

    added spring

---
+++ src/test/java/hif/spengergasse/at/springapidemo/SpringApiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package hif.spengergasse.at.springapidemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringApiDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
