a47e34a8263134b371b4a11d89e2f3266eecab9c
philipp-schaffer <sch19694@spengergasse.at>
2022-09-29 14:01:55 +0200

    added hello world

--- src/main/java/hif/spengergasse/at/springapidemo/SpringApiDemoApplication.java
+++ src/main/java/hif/spengergasse/at/springapidemo/SpringApiDemoApplication.java
@@ -8,6 +8,7 @@ public class SpringApiDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringApiDemoApplication.class, args);
+		System.out.println("Hello world!");
 	}
 
 }
